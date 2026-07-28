49f76d675f8a297790350113a68cdffc2633b6e3
Phelelani Sithole <phelelanisithole33@gmail.com>
2026-07-28 17:30:47 +0200

    Configure Terraform remote backend

--- terraform/bootstrap/providers.tf
+++ terraform/bootstrap/providers.tf
@@ -1,10 +1,23 @@
 terraform {
   required_version = ">= 1.13.0"
 
+  backend "s3" {
+    bucket         = "enterprise-cloud-security-platform-tfstate-640168441707"
+    key            = "bootstrap/terraform.tfstate"
+    region         = "af-south-1"
+    encrypt        = true
+    dynamodb_table = "enterprise-cloud-security-platform-terraform-lock"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.7"
     }
   }
 }
